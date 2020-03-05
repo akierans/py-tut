6cf188475b0759125910aee618e82014acc28c51
Ashley Kierans <akierans@ashleys-mbp.home>
2020-03-05 21:25:26 +0000

    Commit ex 1-3 from lpthw bookmark pg 20

---
+++ lpthw/ex2.py
@@ -0,0 +1,10 @@
+# A comment, thos is so you can read the program later.
+# Anything after the octothorpe is ignored by python.
+
+print ("I could have code like this.") # and the comment after, over here
+
+# You can also use a comment to 'disable' or comment out code, eg.
+
+# print("this print statement has been commented out")
+
+print("But this one has not.")
---
+++ lpthw/ex3.py
@@ -0,0 +1,37 @@
+#Comment that we're going to count chickens
+print("I will now count my chickens:")
+
+#Hen Count
+print("Hens", 25 +30 /6)
+#Rooster Count
+print("Roosters", float(100 - 25 * 3 % 4))
+
+#Comment about to count the eggs
+print("Now I will count the eggs:")
+
+#Print the count only for the number of eggs
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#print the question
+print("Is it true that 3 + 2 < 5 -7")
+
+#Answer the question
+print(3 + 2 < 5 - 7)
+
+#Proof 1
+print("What is 3 + 2?", 3 + 2)
+#Proof 2
+print("What is 5 - 7?", 5 - 7)
+
+#Explain
+print("Oh, that's why it's false")
+
+#More Questions
+print("How about some more.")
+
+#Q1
+print("Is it greater?", 5 > 2)
+#Q2
+print("Is it greater or equal?", 5 >= -2)
+#Q3
+print("Is it less or equal?", 5 <= -2)
