3a72c1f228a5f6acab71d8d4f2fb514d52e3da69
akierans <akierans@gmail.com>
2020-03-24 16:07:46 +0000

    Added comments to ex6

--- lpthw/ex6.py
+++ lpthw/ex6.py
@@ -1,17 +1,23 @@
 types_of_people = 10;
+#String in String
 x = f"There are {types_of_people} types of people."
 
 binary = "binary"
 do_not = "don't"
+#2 Strings inside string
 y = f"Those who know {binary} and those who {do_not}."
 
 print(x)
 print(y)
 
+#String in string
 print(f"I said: {x}")
+
+#String in string
 print(f"I also said: '{y}'")
 
 hilarious = False
+#String in string
 joke_evaluation = "Isn't that joke so funny!? {}"
 print(joke_evaluation.format(hilarious))
 
