1c194ef184ef6288884c4c02701b908737eafedc
akierans <akierans@gmail.com>
2020-03-25 15:42:05 +0000

    Added Ex9 comments explaining

--- lpthw/ex9.py
+++ lpthw/ex9.py
@@ -1,11 +1,15 @@
 #Here's some new strange stuff, remember type it exactly.
 
+#These will appear on the same line
 days = "Mon Tue Wed Thu Fri Sat Sun"
+
+#These will appear line by line because of the \n format
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
 print ("Here are the days: ", days)
 print ("Here are the months: ", months)
 
+#Triple quotes allow to type a text block as is.
 print("""
 There's something going on here.
 With the three double-quotes.
