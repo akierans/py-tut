0b13903ef33a4e3aa42c0440b54928f3839aacd5
akierans <akierans@gmail.com>
2020-03-25 17:21:21 +0000

    Added more escape sequences and commented

--- lpthw/ex10.py
+++ lpthw/ex10.py
@@ -1,9 +1,15 @@
+#\t Add's a TAB
 tabby_cat = "\tI'm tabbed in."
+#\n Starts a new Line
 persian_cat = "I'm a split\non a line."
+#\\ escapes a backslash so it appears in the string
+
 backslash_cat = "I'm \\ a \\ cat."
 
+#\a adds a system bleep
+#\N adds a Unicode symbol where name is in the {} following \N{}
 fat_cat = """
-I'll do a list:
+I'll do a list\N{COLON}
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
