fb725f8ec2b69437acfda909ad23d39676bcc596
akierans <akierans@gmail.com>
2020-03-31 17:17:40 +0100

    submitted ex 13a.py

---
+++ lpthw/ex13a.py
@@ -0,0 +1,11 @@
+from sys import argv
+#Read the WYSS section for how to run this
+
+script, age, height, weight, eyes, hair = argv
+
+print ("You are ", age)
+print ("You are ", height,"cm tall.")
+print ("You are ", weight,"kg heavy.")
+print ("You have ",eyes, " colour eyes.")
+print ("You have ", hair," colour hair.")
+
