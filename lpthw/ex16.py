46436e12083aeb2decce70ece436cacfb21911aa
akierans <akierans@gmail.com>
2020-04-02 15:27:05 +0100

    Added comments and updated sample after running

--- lpthw/ex16.py
+++ lpthw/ex16.py
@@ -1,27 +1,34 @@
 from sys import argv
 
+#Pulls params from commanline input
 script, filename = argv
 
+#Explains what we're going to do instructing hwow to interrupt
 print(f"We're going to erase {filename}.")
 print("If you don't want that, hit CTRL-C (^C).")
 print("If you do want that, hit RETURN.")
 
+#Accepts input
+#Not validated for return only!
 input("?")
 
 print("Opening the file...")
+#sets target as filename
 target = open(filename, 'w')
 
+#Truncates File
 print("Truncating the file. Goodbye!")
 target.truncate()
 
 print("Now I'm going to ask you for three line.")
-
+#Accepts New input 3 lines
 line1 = input("line 1: ")
 line2 = input("Line 2: ")
 line3 = input("Line 3: ")
 
 print("I'm going to write these lines to the file.")
 
+#Writes lines one at a time with a new line escape character between
 target.write(line1)
 target.write("\n")
 target.write(line2)
@@ -30,4 +37,5 @@ target.write(line3)
 target.write("\n")
 
 print("And finally, we close it.")
+#Closes once complete
 target.close()
