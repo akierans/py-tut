b2a9fed42abb8a2a80eaa8866576cc6eaa4157ea
akierans <akierans@gmail.com>
2020-06-15 15:09:30 +0100

    Added Ex 47 and directory including Automated Tests

---
+++ lpthw/ex47/setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setuptools
+except ImportError:
+	from distutils.core import setuptools
+
+config = {
+	'description': 'My Project',
+	'author': 'Ash Kierans',
+	'url': 'https://github.com/akierans/py-tut'
+	'download_url': 'https://github.com/akierans/py-tut'
+	'author_email': 'akierans@gmail.com'
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['ex47'],
+	'scripts': [],
+	'name': 'projectname'
+}
+
+setup(**config)
