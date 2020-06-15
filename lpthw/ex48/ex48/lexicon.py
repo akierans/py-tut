055c71392cc613c5d3f6488a1a462f7a945f642d
akierans <akierans@gmail.com>
2020-06-15 19:11:57 +0100

    Added first class, still need to fix errors

--- lpthw/ex48/ex48/lexicon.py
+++ lpthw/ex48/ex48/lexicon.py
@@ -1,12 +1,27 @@
-class Room(object):
+directions = {' north', 'south', 'east', 'west', 'up', 'down', 'left', 'right', 'back', 'forward'}
+verbs = {'go', 'stop', 'kill', 'eat'}
+stop_words = {'the', 'in', 'from', 'of', 'at', 'it'}
+nouns = {'door', 'bear', 'princess', 'cabinet'}
+#numbers = {}
 
-	def __init__(self, name, description):
-		self.name = name
-		self.description = description
-		self.paths = {}
+def scan(words):
+	
+	words = words.split()
+	sentence = []
 
-	def go(self, direction):
-		return self.paths.get(direction, None)
+	for i in directions:
+		if i in directions:
+			sentence.append(('direction', i))
 
-	def add_paths(self, paths):
-		self.paths.update(paths)
+		elif i in verbs:
+			sentence.append(('verb', i))
+
+		elif i in stop_words:
+			sentence.append(('stop', i))
+
+		elif i in nouns:
+			sentence.append(('noun', i))
+		else:
+			sentence.append(('error', i))
+
+	return sentence
