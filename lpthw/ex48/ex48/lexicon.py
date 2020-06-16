e1ea7847e856a2833d9016dd838d2053466e8859
akierans <akierans@gmail.com>
2020-06-16 22:02:06 +0100

    ex48 now passes 5/6 unittests

--- lpthw/ex48/ex48/lexicon.py
+++ lpthw/ex48/ex48/lexicon.py
@@ -1,27 +1,39 @@
-directions = {' north', 'south', 'east', 'west', 'up', 'down', 'left', 'right', 'back', 'forward'}
+directions = {'north', 'south', 'east', 'west', 'up', 'down', 'left', 'right', 'back', 'forward'}
 verbs = {'go', 'stop', 'kill', 'eat'}
 stop_words = {'the', 'in', 'from', 'of', 'at', 'it'}
 nouns = {'door', 'bear', 'princess', 'cabinet'}
 #numbers = {}
+
 
 def scan(words):
 	
 	words = words.split()
 	sentence = []
 
-	for i in directions:
-		if i in directions:
+	for i in words:
+		if i.lower() in directions:
 			sentence.append(('direction', i))
 
-		elif i in verbs:
+		elif i.lower() in verbs:
 			sentence.append(('verb', i))
 
-		elif i in stop_words:
+		elif i.lower() in stop_words:
 			sentence.append(('stop', i))
 
-		elif i in nouns:
+		elif i.lower() in nouns:
 			sentence.append(('noun', i))
+		
+		elif i.isdigit():
+			sentence.append(('number', convert_number(i)))
+
 		else:
 			sentence.append(('error', i))
 
 	return sentence
+
+def convert_number(s):
+	try:
+		return int(s)
+
+	except ValueError:
+		return None
