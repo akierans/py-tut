10c9bef5c5456a2059e7eabc443a56c16b962788
akierans <akierans@gmail.com>
2020-06-22 17:51:07 +0100

    Added tests to 4x49 for parse verb and match

--- lpthw/ex49/tests/parser_tests.py
+++ lpthw/ex49/tests/parser_tests.py
@@ -6,17 +6,17 @@ from copy import deepcopy
 def test_peek():
 	assert_equal(parser.peek(lexicon.scan("north south east west")), "direction")
 	assert_equal(parser.peek(lexicon.scan("moo ias blargh")), 'error')
-	result = parser.peek(lexicon.scan("north, south, east, west, up, downn, left, right"))
-	assert_equal(result, [('direction', 'north'),
-							('direction', 'south'),
-							('direction', 'east'),
-							('direction', 'west'),
-							('direction', 'up'),
-							('direction', 'left'),
-							('direction', 'right')])
-
+	#result = parser.peek(lexicon.scan("north, south, "))
+	result = parser.peek(lexicon.scan("north south east west up downn left right"))
+	assert_equal(result, 'direction')
 
 def test_match():
  	assert_equal(parser.match(lexicon.scan("kill stop bear"), "verb"), ('verb', 'kill'))
-# 	assert_equal(parser.match(lexicon.scan("kill stop bear"), "stop"), None)
+ 	assert_equal(parser.match(lexicon.scan("kill stop bear"), "stop"), None)
+ 	assert_equal(parser.match(lexicon.scan("bear walks in woods"), "noun"), ('noun', 'bear'))
 
+def test_pverb():
+	assert_equal(parser.parse_verb(lexicon.scan("eat it")), ('verb', 'eat'))
+	assert_equal(parser.parse_verb(lexicon.scan("in the eat")), ('verb', 'eat'))
+	assert_equal(parser.parse_verb(lexicon.scan("of the kill")), ('verb', 'kill'))
+	assert_raises(Exception, parser.parse_verb, lexicon.scan("princess ate the bear"))
